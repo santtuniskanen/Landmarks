06debe7a487a00d9f84a221d5b00293e2cab73cb
santtuniskanen <santtuniska02@gmail.com>
2024-03-02 13:39:15 +0200

    added badge background

---
+++ Landmarks/Landmarks/Views/BadgeBackground.swift
@@ -0,0 +1,18 @@
+//
+//  BadgeBackground.swift
+//  Landmarks
+//
+//  Created by santtuniskanen on 2.3.2024.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BadgeBackground()
+}
