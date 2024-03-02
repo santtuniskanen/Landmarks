fd3ed0eedeafdd99fb32fa08ef2ee0b6cf64b000
santtuniskanen <santtuniska02@gmail.com>
2024-03-02 21:53:34 +0200

    added watch view

---
+++ Landmarks/WatchLandmarks Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Watch App
+//
+//  Created by santtuniskanen on 2.3.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
