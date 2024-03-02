ffd161833e22260a56b638d5c16ee7e142a35959
santtuniskanen <santtuniska02@gmail.com>
2024-03-02 20:47:17 +0200

    fixed bugs

--- Landmarks/Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Landmarks/Views/Categories/CategoryHome.swift
@@ -7,15 +7,14 @@
 
 import SwiftUI
 
-
 struct CategoryHome: View {
     @Environment(ModelData.self) var modelData
     @State private var showingProfile = false
-    
+
     var body: some View {
         NavigationSplitView {
             List {
-                PageView(pages: modelData.features.map { FeatureCard(landmark: $0)})
+                PageView(pages: modelData.features.map { FeatureCard(landmark: $0) })
                     .listRowInsets(EdgeInsets())
                 
                 ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
@@ -25,17 +24,22 @@ struct CategoryHome: View {
             }
             .listStyle(.inset)
             .navigationTitle("Featured")
-            Button {
-                showingProfile.toggle()
-            } label: {
-                Label("User Profile", systemImage: "person.crop.circle")
-            }   
+            .toolbar {
+                Button {
+                    showingProfile.toggle()
+                } label: {
+                    Label("User Profile", systemImage: "person.crop.circle")
+                }
+            }
+            .sheet(isPresented: $showingProfile) {
+                ProfileHost()
+                    .environment(modelData)
+            }
         } detail: {
             Text("Select a Landmark")
         }
     }
 }
-
 
 #Preview {
     CategoryHome()
