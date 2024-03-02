fdd5b0c1e18be85531c25606c6dda37c906616d3
santtuniskanen <santtuniska02@gmail.com>
2024-03-02 19:54:24 +0200

    finished edit page

--- Landmarks/Landmarks/Views/Profiles/ProfileEditor.swift
+++ Landmarks/Landmarks/Views/Profiles/ProfileEditor.swift
@@ -9,6 +9,12 @@ import SwiftUI
 
 struct ProfileEditor: View {
     @Binding var profile: Profile
+    
+    var dateRange: ClosedRange<Date> {
+        let min = Calendar.current.date(byAdding: .year, value: -1, to: profile.goalDate)!
+        let max = Calendar.current.date(byAdding: .year, value: 1, to: profile.goalDate)!
+        return min...max
+    }
     var body: some View {
         List {
             HStack {
